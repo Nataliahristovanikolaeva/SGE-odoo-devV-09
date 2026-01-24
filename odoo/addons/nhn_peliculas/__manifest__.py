8d45a423fc8bdff8220dae5476718aa451e95304
Natalia Hristova <petiovahristovan@gmail.com>
2026-01-24 18:32:29 +0100

    Commit3b

--- odoo/addons/nhn_peliculas/__manifest__.py
+++ odoo/addons/nhn_peliculas/__manifest__.py
@@ -14,6 +14,7 @@ Long description of module's purpose
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
+    #mmmmmm
     'category': 'Uncategorized',
     'version': '0.1',
 
